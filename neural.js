f8ebabb42492b8419409cf425059e5c6a9453125
Paulo Augusto Teixeira <pauloaugustot@gmail.com>
2018-08-29 15:11:39 -0300

    Update neural.js

--- neural.js
+++ neural.js
@@ -1,10 +1,10 @@
-var brain = require("./node_modules/brain/lib/brain");
-var network = new brain.NeuralNetwork();
-var training = [];
+const brain = require("./node_modules/brain/lib/brain");
+let network = new brain.NeuralNetwork();
+let training = [];
 
 module.exports = {
   neuralTraining: function (obj) {
-      for( i=0; i<100; i++ ) {
+      for( let i=0; i<100; i++ ) {
           var width = Math.floor(Math.random() * 10000);
           var height = Math.floor(Math.random() * 10000);
           training[i] = (width > height) ?
